e3a8d150f03fe728db2eae64ef7fcb565d96eb3c
trela <trela.art@gmail.com>
2023-09-04 22:56:07 +0200

    adding automatic update value in mysql db

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ import pymysql
 
 min_gen_value = 2200
 max_gen_value = 2300
-test_value = 0
+voltage_L1 = 0
 curr_L1_min = 0
 curr_L1_max = 160
 current_L1 = 0
@@ -27,35 +27,34 @@ sample_time_resolution = 2
 timer = dt.datetime.now()
 now = timer.strftime("%Y-%m-%d /%H:%M:%S  ")
 measureTable = []
+act_hours = timer.strftime("%H:%M:%S")
+act_date = timer.date()
 # save_file_name = 'measureTable/measureValue '+str(dt.datetime.now().strftime("%Y-%m-%d %H-%M-%S"))+'.csv'
 save_file_name = 'MeasureValue.csv'
 print(now)
 
-
-
-#
 while condition:
 
     delta = dt.datetime.now() - timer
     if delta.seconds >= sample_time_resolution:
-        test_value = random.randint(min_gen_value, max_gen_value)
+        voltage_L1 = random.randint(min_gen_value, max_gen_value)
         last_record = dt.datetime.now()
         last_record = last_record.strftime("%Y-%m-%d godz. %H:%M:%S  ")
-        test_value = test_value / 10
+        voltage_L1 = voltage_L1 / 10
         current_L1 = random.randint(curr_L1_min, curr_L1_max)
         current_L1 = current_L1 / 10
         timer = dt.datetime.now()
         sample_count += 1
-        sample_package = [(sample_count), (last_record) , (test_value), (current_L1)]
+        sample_package = [(sample_count), (last_record) , (voltage_L1), (current_L1)]
         measureTable.append(sample_package)
         print(
-            f' [{sample_count}] Pomiar  w dniu : {last_record} wykazał {test_value}V dla L1 a prąd zarejestrowany {current_L1}A')
+            f' [{sample_count}] Data pomiaru : {act_date} godz, {act_hours} wykazał {voltage_L1}V dla L1 a prąd zarejestrowany {current_L1}A')
 
     def filesaver():
         if sample_count == 10:
-            for i in range(0, sample_count):
-                print(measureTable[i])
-                i +=1
+            # for i in range(0, sample_count):
+            #     print(measureTable[i])
+            #     i +=1
 
             with open(save_file_name, 'w', encoding='UTF8',newline='') as f:
                 header = ['No', 'Date&Hour', 'Measure Valve 1 ', 'Measure Value 2']
@@ -69,7 +68,7 @@ while condition:
                 writer.writerows(data)
 
     filesaver()
-    if sample_count == 10:
+    if sample_count == 2:
         condition = False
 
 
@@ -81,15 +80,30 @@ def mysqlconnect():
         password="malina",
         db='rpi_db',
     )
-
+    ask_query = 'SELECT measure_count , L1_Current , L1_Voltage from last_values '
     cur = conn.cursor()
-    cur.execute("select @@version")
+    # cur.execute(f"select @@version")
+    # cur.execute(ask_query)
+
+    # for (measure_count, L1_Current, L1_Voltage ) in cur:
+    #      print(f'{measure_count} - {L1_Current}  & {L1_Voltage}')
+    #
+    insert_query = 'INSERT INTO last_values (measure_count , L1_Current , L1_Voltage)' \
+                   ' VAlUES(%(measure_count)s, %(L1_Current)s,%(L1_Voltage)s)'
+    insertData = {
+         'measure_count': 0,
+        'L1_Current': current_L1,
+        'L1_Voltage': voltage_L1,
+    }
+    cur.execute(insert_query, insertData)
     output = cur.fetchall()
-
     print(output)
+    print(act_hours)
+    print(act_date)
+    conn.commit()
     conn.close()
-
 
 if __name__ == "__main__":
     mysqlconnect()
+
 
